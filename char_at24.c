87a172279072e0d9d9e65495329df103ce919775
Reyders Morales <reyders1@gmail.com>
2025-01-12 18:37:09 +0100

    Refactor read and write methods

--- char_at24.c
+++ char_at24.c
@@ -11,12 +11,9 @@
 
 /* Address pointer is 16 bit. */
 #define AT24_FLAG_ADDR16	BIT(7)
-/* sysfs-entry will be read-only. */
-#define AT24_FLAG_READONLY	BIT(6)
-/* sysfs-entry will be world-readable. */
-#define AT24_FLAG_IRUGO		BIT(5)
 
 #define AT24_24C256_LEN  (262144 / 8)
+#define MAX_READ_LEN     1500
 
 struct at24_data {
     u32 byte_len;
@@ -53,21 +50,30 @@ static ssize_t char_at24_read(struct file *filp, char __user *buff, size_t count
 {
     struct at24_data *data = filp->private_data;
     struct i2c_client *client = data->client;
-    u8 kbuff[at24_io_limit];
     u8 i;
     s32 res;
+    ssize_t ret;
+    char *src_buf;
+    u8 addr_hi = (*f_pos >> 8) & 0xFF;
+    u8 addr_lo = *f_pos & 0xFF;
 
-    if (mutex_lock_interruptible(&data->lock))
-        return -EINTR;
+    src_buf = devm_kzalloc(data->device, at24_io_limit, GFP_KERNEL);
+    if (!src_buf)
+        return -ENOMEM;
+
+    if (mutex_lock_interruptible(&data->lock)) {
+        ret = -EINTR;
+        goto clean;
+    }
 
     pr_info("Read requested for %zu bytes \n", count);
 	pr_info("Current file position = %lld\n", *f_pos);
 
     /* early exit */
-    if (*f_pos != 0) {
+    if (*f_pos >= MAX_READ_LEN) {
         *f_pos = 0;
-        mutex_unlock(&data->lock);
-        return 0;
+        ret = 0;
+        goto unlock;
     }
 
     /* Adjust the count*/
@@ -76,25 +82,29 @@ static ssize_t char_at24_read(struct file *filp, char __user *buff, size_t count
     }
     count = at24_io_limit;
 
-    res = i2c_smbus_write_byte_data(client, 0x00, 0x00);
+    /* Random read: dummy write to the address that we want to read*/
+    res = i2c_smbus_write_byte_data(client, addr_hi, addr_lo);
     if (res < 0) {
-        pr_err("error reading i2c_smbus_write_byte_data\n");
+        pr_err("error dummy write i2c_smbus_write_byte_data\n");
+        ret = -EINTR;// @fix error
+        goto unlock;
     }
 
-    for(i = 0; i < at24_io_limit; i++) {
+    for (i = 0; i < count; i++) {
         res = i2c_smbus_read_byte(client);
         if (res < 0) {
             pr_err("error reading\n");
-            return -EINTR; // @fix error
+            ret = -EINTR;// @fix error
+            goto unlock;
         }
-        kbuff[i] = (u8)res;
+        src_buf[i] = (u8)res;
     }
 
-    kbuff[at24_io_limit - 1] = '\n';
+    src_buf[count - 1] = '\n';
 
-    if (copy_to_user(buff, &kbuff[0], count)) {
-        mutex_unlock(&data->lock);
-        return -EFAULT;
+    if (copy_to_user(buff, &src_buf[0], count)) {
+        ret = -EFAULT;
+        goto unlock;
     }
 
     *f_pos += count;
@@ -103,41 +113,60 @@ static ssize_t char_at24_read(struct file *filp, char __user *buff, size_t count
     pr_info("Updated file position = %lld\n", *f_pos);
 
     mutex_unlock(&data->lock);
+    devm_kfree(data->device, src_buf);
 
     return count;
+
+unlock:
+    mutex_unlock(&data->lock);
+clean:
+    devm_kfree(data->device, src_buf);
+    return ret;
 }
 
 static ssize_t char_at24_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
 {
     struct at24_data *data = filp->private_data;
     struct i2c_client *client = data->client;
-    u8 kbuff[at24_io_limit];
     u8 addr_hi, addr_lo, i;
     u16 address;
     s32 res;
+    char *dst_buf;
+    ssize_t ret;
 
-    if (mutex_lock_interruptible(&data->lock))
-        return -EINTR;
+    dst_buf = devm_kzalloc(data->device, at24_io_limit, GFP_KERNEL);
+    if (!dst_buf)
+        return -ENOMEM;
+
+    if (count > at24_io_limit)
+        count = at24_io_limit;
+
+    if (*f_pos + count >  AT24_24C256_LEN)
+        return -ENOMEM;
+
+    if (mutex_lock_interruptible(&data->lock)) {
+        ret = -EINTR;
+        goto clean;
+    }
 
     pr_info("Write requested for %zu bytes\n", count);
     pr_info("Current file position = %lld\n", *f_pos);
 
-    count = at24_io_limit;
     /*copy from user*/
-    if (copy_from_user(&kbuff[0], buff, count)) {
-        mutex_unlock(&data->lock);
-        return -EFAULT;
+    if (copy_from_user(&dst_buf[0], buff, count)) {
+        ret = -EFAULT;
+        goto unlock;
     }
 
-    address = 0;
+    address = *f_pos;
     for (i = 0; i < at24_io_limit; i++) {
         addr_hi = address >> 8;
         addr_lo = address;
-        res = i2c_smbus_write_word_data(client, addr_hi, (kbuff[i] << 8 | addr_lo));
+        res = i2c_smbus_write_word_data(client, addr_hi, (dst_buf[i] << 8 | addr_lo));
         if (res < 0) {
             pr_err("error writing \n");
-            mutex_unlock(&data->lock);
-            return -EFAULT;
+            ret = -EFAULT;
+            goto unlock;
         }
         address++;
         usleep_range(4000, 4500);
@@ -148,7 +177,15 @@ static ssize_t char_at24_write(struct file *filp, const char __user *buff, size_
 
     mutex_unlock(&data->lock);
 
+    devm_kfree(data->device, dst_buf);
+
     return count;
+
+unlock:
+    mutex_unlock(&data->lock);
+clean:
+    devm_kfree(data->device, dst_buf);
+    return ret;
 }
 
 static int char_at24_open(struct inode *inode, struct file *filp)
